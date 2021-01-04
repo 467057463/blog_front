fef75619af251666c02f470845815d18d7e03536
m2 <467057463@qq.com>
2021-01-04 18:04:42 +0800

    change style

--- src/components/CoderEditor.js
+++ src/components/CoderEditor.js
@@ -5,18 +5,20 @@ import 'codemirror/mode/markdown/markdown'
 
 export default ({content, setContent}) => {
   return(
-    <CodeMirror
-      value={content}
-      options={{
-        mode: 'markdown',
-        theme: 'material-palenight',
-        lineNumbers: true
-      }}
-      onBeforeChange={(editor, data, value) => {
-        setContent(value);
-      }}
-      onChange={(editor, data, value) => {
-      }}
-    />
+    <div className='coder-editor'>
+      <CodeMirror
+        value={content}
+        options={{
+          mode: 'markdown',
+          theme: 'material-palenight',
+          lineNumbers: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setContent(value);
+        }}
+        onChange={(editor, data, value) => {
+        }}
+      />
+    </div>
   )
 }
