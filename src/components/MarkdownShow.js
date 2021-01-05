924719ba8ae033ab6a5b66a47ef85052622018e2
m2 <467057463@qq.com>
2021-01-05 22:08:20 +0800

    前端 markdown 目录方案

--- src/components/MarkdownShow.js
+++ src/components/MarkdownShow.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
+import * as slug from 'remark-slug';
+import HeadingRenderer from '@/components/HeadingRenderer';
+
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {pojoaque} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const renderers = {
+
   code: ({language, value}) => {
     return <SyntaxHighlighter style={pojoaque} language={language} children={value} />
   }
@@ -15,7 +19,7 @@ export default ({content}) => {
     <ReactMarkdown        
       className='markdown-body'
       plugins={[gfm]} 
-      renderers={renderers} 
+      renderers={{'heading': HeadingRenderer, ...renderers}}
       children={content} 
     />
   )
