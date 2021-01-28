500f297adf9835942b27ddc3dce94522df96535c
m2 <467057463@qq.com>
2021-01-28 14:12:12 +0800

    fix code line hight

--- src/components/MarkdownShow copy.js
+++ src/components/MarkdownShow copy.js
@@ -9,17 +9,6 @@ import {a11yDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
 // import SyntaxHighlighter from 'react-syntax-highlighter';
 // import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const ADDED = [1, 2];
-const REMOVED = [6];
-
-function scopeNumber(start, end){
-  let res = []
-  for(let i = start; i <= end; i++){
-    res.push(i)
-  }
-  return res;
-}
-
 const renderers = {
   tableRender(props){
     return (
@@ -29,37 +18,14 @@ const renderers = {
     )
   },
   code: ({language, value}) => {
-    const reg = /{(.+)}$/;
-    const scopeReg = /^(\d+)-(\d+)$/;
-    const line = language.match(reg);
-    let lines = []
-    if(line){
-      line[1].split(",").forEach(item =>{
-        const match = item.match(scopeReg);
-        if(match){
-          lines = lines.concat(scopeNumber(+match[1], +match[2]))
-        }else{
-          lines.push(+item)
-        }
-      })
-    }
     return (
       <SyntaxHighlighter 
         showLineNumbers 
         showInlineLineNumbers 
         style={a11yDark} 
-        language={language.replace(reg, '')}
-        wrapLines
-        lineProps={lineNumber => {
-          let style = { display: 'block' };
-          if (lines.includes(lineNumber)) {
-            style.backgroundColor = '#404040';
-          }
-          return { style };
-        }} 
-      >
-        {value}
-      </SyntaxHighlighter>
+        language={language} 
+        children={value} 
+      />
     )
   },
   heading: HeadingRenderer
